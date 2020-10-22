cb17b5c95e0f6d110fac6c6d87a8659bc70245bc
MrAlligator <rizkiw8778@gmail.com>
2020-10-22 21:45:18 +0700

    Start Backend

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,5 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['user'] = 'user/overview';
+
+$route['admin'] = 'admin/dashboard';
--- application/views/user/_partials/head.php
+++ application/views/user/_partials/head.php
@@ -1,12 +1,3 @@
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title> Unistore Pro &middot; Premium Responsive E-Commerce Template</title>
-
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="Sunrise Digital">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('assets/favicon.png') ?>">
 
     <!-- Bootstrap -->
