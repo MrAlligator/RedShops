8af35f472787b8c9003c75580e6a2252c5fe5003
Obaysavage123 <ilmi.obbi@gmail.com>
2020-12-10 05:42:42 -0800

    Awkawkak

--- application/views/user/_partials/header.php
+++ application/views/user/_partials/header.php
@@ -8,21 +8,21 @@
 
           <div class="item center" data-marker="1">
             <img src="<?php echo base_url('assets/img/carousel/bg/bgredshop.png') ?>" alt="Background" class="background hidden-xs hidden-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-2-sm.jpg') ?>" alt="Background" class="background visible-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-2-xs.jpg') ?>" alt="Background" class="background visible-xs"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-sm.png') ?>" alt="Background" class="background visible-sm"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-lg.png') ?>" alt="Background" class="background visible-xs"/>
 
             <div class="content">
               <div class="outside-content">
                 <div class="inside-content">
                   <div class="container align-right">
 
-                    <h1 class="h3 colorful blue hidden-xs">Provide lightweight and powerull</h1>
+                    <h1 class="h3 colorful blue hidden-xs"></h1>
 
                     <hr class="offset-sm">
-                    <h2 class="h1 lg upp colorful blue">New amazing <br> laptops</h2>
+                    <h2 class="h1 lg upp colorful blue"> <br> </h2>
                     <hr class="offset-md">
                     <hr class="offset-md">
-                    <a href="./store/" rel="nofollow" class="btn btn-primary btn-lg black"> More laptops </a>
+  
 
                   </div>
                 </div>
@@ -31,9 +31,9 @@
           </div>
           
           <div class="item" data-marker="2">
-          <img src="<?php echo base_url('assets/img/carousel/bckg-1.jpg') ?>" alt="Background" class="background hidden-xs hidden-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-1-sm.jpg') ?>" alt="Background" class="background visible-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-1-xs.jpg') ?>" alt="Background" class="background visible-xs"/>
+          <img src="<?php echo base_url('assets/img/carousel/bg/bgredshop.png') ?>" alt="Background" class="background hidden-xs hidden-sm"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-sm.png') ?>" alt="Background" class="background visible-sm"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-lg.png') ?>" alt="Background" class="background visible-xs"/>
 
             <!-- <img src="assets/img/carousel/newlaptops.jpg" alt="New laptops" title="New laptops" class="item-left hidden-xs"/> -->
 
@@ -42,13 +42,13 @@
                 <div class="inside-content">
                   <div class="container">
 
-                    <h1 class="h3 colorful blue hidden-xs">Device is designed for the creative people</h1>
+                    <h1 class="h3 colorful blue hidden-xs"></h1>
                     <hr class="offset-sm">
 
-                    <h2 class="h1 lg upp colorful blue">Apple <br> iMac 27 Retina</h2>
+                    <h2 class="h1 lg upp colorful blue"> <br> </h2>
                     <hr class="offset-md">
                     <hr class="offset-md">
-                    <a href="./store/" rel="nofollow" class="btn btn-primary btn-lg black"> View products </a>
+                   
                     
                   </div>
                 </div>
@@ -57,9 +57,9 @@
           </div>
 
           <div class="item" data-marker="3">
-          <img src="<?php echo base_url('assets/img/carousel/bckg-3.jpg') ?>" alt="Background" class="background hidden-xs hidden-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-3-sm.jpg') ?>" alt="Background" class="background visible-sm"/>
-            <img src="<?php echo base_url('assets/img/carousel/bckg-3-xs.jpg') ?>" alt="Background" class="background visible-xs"/>
+          <img src="<?php echo base_url('assets/img/carousel/bg/bgredshop.png') ?>" alt="Background" class="background hidden-xs hidden-sm"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-sm.png') ?>" alt="Background" class="background visible-sm"/>
+            <img src="<?php echo base_url('assets/img/carousel/bgredshop-lg.png') ?>" alt="Background" class="background visible-xs"/>
 
             <!-- <img src="assets/img/carousel/ipadair2.jpg" alt="Apple Devices" title="Apple Devices" class="item-right hidden-xs"/> -->
 
@@ -68,13 +68,13 @@
                 <div class="inside-content">
                   <div class="container align-right">
                   
-                    <h1 class="h3 colorful blue hidden-xs">Luxury watches, business tablets and 3D touch: <br> How Apple plans to stay ahead in mobile.</h1>
+                    <h1 class="h3 colorful blue hidden-xs"><br> </h1>
 
                     <hr class="offset-sm">
-                    <h2 class="h1 lg upp colorful blue">Brand new day <br class="hidden-xs"> for business</h2>
+                    <h2 class="h1 lg upp colorful blue"><br class="hidden-xs"> </h2>
                     <hr class="offset-md">
                     <hr class="offset-md">
-                    <a href="./blog/" rel="nofollow" class="btn btn-primary btn-lg black"> View article </a>
+                    <a href="./blog/" rel="nofollow" class="btn btn-primary btn-lg black">  </a>
                       
                   </div>
                 </div>
