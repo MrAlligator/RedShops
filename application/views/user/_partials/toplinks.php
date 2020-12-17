73160d431047e70e21873bc5b1c3a56a82a87ca2
Achmad Syadidul Fahim <55520229+fahim@users.noreply.github.com>
2020-12-17 23:48:57 +0700

    Update toplinks.php

--- application/views/user/_partials/toplinks.php
+++ application/views/user/_partials/toplinks.php
@@ -1,11 +1,11 @@
 <div class="toplinks">
-<?php if ($user['username']) : ?>
-    <a href="#signin" data-toggle="modal" data-target="#logoutModal"> <i class="ion-unlocked"></i> Logout</a>
-    <a href="tel:+80005554465" class="hidden-xs"> <i class="ion-android-call"></i> 8000 555-44-65 </a>
-    <a href="<?= base_url("user/profile")?>" class="hidden-xs"> <i class="ion-person"></i> Hai, <?= $user['name']; ?></a>
-<?php else : ?>
-    <a href="#signin" data-toggle="modal" data-target="#Modal-Registration"> <i class="ion-person"></i> Daftar</a>
-    <a href="#signin" data-toggle="modal" data-target="#Modal-SignIn"> <i class="ion-locked"></i> Masuk</a>
-    <a href="tel:+80005554465" class="hidden-xs"> <i class="ion-android-call"></i> 8000 555-44-65 </a>
-<?php endif; ?>
+    <?php if (isset($user['username'])) : ?>
+        <a href="#signin" data-toggle="modal" data-target="#logoutModal"> <i class="ion-unlocked"></i> Logout</a>
+        <a href="tel:+80005554465" class="hidden-xs"> <i class="ion-android-call"></i> 8000 555-44-65 </a>
+        <a href="<?= base_url("user/profile") ?>" class="hidden-xs"> <i class="ion-person"></i> Hai, <?= $user['name']; ?></a>
+    <?php else : ?>
+        <a href="#signin" data-toggle="modal" data-target="#Modal-Registration"> <i class="ion-person"></i> Daftar</a>
+        <a href="#signin" data-toggle="modal" data-target="#Modal-SignIn"> <i class="ion-locked"></i> Masuk</a>
+        <a href="tel:+80005554465" class="hidden-xs"> <i class="ion-android-call"></i> 8000 555-44-65 </a>
+    <?php endif; ?>
 </div>
