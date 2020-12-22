bf1ca9dec7234c965c9465231e0bc38bd2436caf
mycojihan13 <pascawiyata29@gmail.com>
2020-12-22 13:27:27 +0700

    Update dashboard.php

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -78,7 +78,7 @@
             </div>
 
             <!-- Pending Requests Card Example -->
-            <!--<div class="col-xl-3 col-md-6 mb-4">
+            <<div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -89,7 +89,7 @@
                     <div class="col-auto">
                       <i class="fa fa-shopping-cart text-gray-300"></i>
                     </div>-->
-                  <!-- </div> 
+                  </div> 
                 </div>
               </div>
             </div> -->
